dfe35b6bc14ac821bffe703eb8885f3098b680ef
Pujan <pujan3892@gmail.com>
2019-03-19 15:16:07 -0500

    Nav bar scaffolding

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import "./app.css"
 
 import Home from "./modules/Home";
 import Plan from "./modules/Plan"
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,30 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
+import "./navigation.css"
 
 const Navigation = () => {
   return (
     <div>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/plan">Plan</NavLink>
-      <NavLink to="/explore">Explore</NavLink>
+      <header className="header">
+        <div className="logo">Hell of a ride</div>
+        <input
+          className="menu-btn"
+          type="checkbox"
+          id="menu-btn"
+        />
+        <label
+          className="menu-icon"
+          for="menu-btn"
+        >
+          <span className="navicon">
+          </span>
+        </label>
+        <ul className="menu">
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/plan">Plan</NavLink></li>
+          <li><NavLink to="/explore">Explore</NavLink></li>
+        </ul>
+      </header>
     </div>
   );
 };
