d0b4e82542de7a921507cd63e8aca195485d7963
Pujan <pujan3892@gmail.com>
2019-04-16 23:32:51 -0500

    google maps add

--- client/src/components/Maps.js
+++ client/src/components/Maps.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
-const Maps = () => {
-  return (
-    <div>
-      <p>sup</p>
-    </div>
-  );
-};
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class Maps extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyBmy6fB8rpp0Nb7TSHGdfnfH8TQyMBayrQ' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
 
 export default Maps;
